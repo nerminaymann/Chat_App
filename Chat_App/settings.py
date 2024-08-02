d36834435063f69452eeaff1aa25344b63c8d84f
Nermin Ayman <45697519+nerminaymann@users.noreply.github.com>
2024-08-02 21:56:33 +0300

    add Reconnecting WebSocket and configured it in static files

--- Chat_App/settings.py
+++ Chat_App/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 from django.core.management.commands.runserver import Command as rs
 rs.default_port='5000'
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
