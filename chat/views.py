2a10d9442829cc0f409816a784dac50f8b16a964
Nermin Ayman <45697519+nerminaymann@users.noreply.github.com>
2024-08-03 22:59:19 +0300

    add username and updated room page

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 # Create your views here.
@@ -9,6 +10,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, "chat/index.html")
 
+@login_required
 def room(request, room_name):
-    return render(request, "chat/room.html", {"room_name_json": mark_safe(json.dumps(room_name))
-                                              })
+    return render(request, "chat/room.html",
+                  {"room_name_json": mark_safe(json.dumps(room_name)),
+                   "username": mark_safe(json.dumps(request.user.username)) })
